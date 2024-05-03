5f187f31ab0966d3419eb0ddfe51c8885bcc29f6
canto1380 <atapenalba16@gmail.com>
2024-05-03 19:42:49 -0300

    se agrego fechas destileria en periodos

--- src/components/PeriodosZafra/FormAddEditPeriodos.js
+++ src/components/PeriodosZafra/FormAddEditPeriodos.js
@@ -1,9 +1,9 @@
-import { Form, Select, Spin, Button, Row, Col } from "antd";
-import "./periodoZafra.css";
-import { useState } from "react";
-import { api } from "../../utils/api";
-import MsgError from "../Messages/MsgError";
-import moment from "moment";
+import { Form, Select, Spin, Button, Row, Col } from 'antd'
+import { useState } from 'react'
+import { api } from '../../utils/api'
+import MsgError from '../Messages/MsgError'
+import moment from 'moment'
+import './periodoZafra.css'
 
 const FormAddEditPeriodos = ({
   dataRegisterEdit,
@@ -12,155 +12,167 @@ const FormAddEditPeriodos = ({
   aniosData,
   ingeniosData,
 }) => {
-  const [dataError, setDataError] = useState(false);
-  const [messageError, setMessageError] = useState("");
-  const [dataErrorPK, setDataErrorPK] = useState(false);
-  const [errorPKDuplicate, setErrorPKDuplicate] = useState("");
-  const [serverError, setServerError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [dataError, setDataError] = useState(false)
+  const [messageError, setMessageError] = useState('')
+  const [dataErrorPK, setDataErrorPK] = useState(false)
+  const [errorPKDuplicate, setErrorPKDuplicate] = useState('')
+  const [serverError, setServerError] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [inicioZafra, setInicioZafra] = useState(dataRegisterEdit?.inicio_zafra)
   const [finZafra, setFinZafra] = useState(dataRegisterEdit?.fin_zafra)
+  const [inicioDestileria, setInicioDestileria] = useState(
+    dataRegisterEdit?.inicio_destileria
+  )
+  const [finDestileria, setFinDestileria] = useState(
+    dataRegisterEdit?.fin_destileria
+  )
 
-  const [form] = Form.useForm();
+  const [form] = Form.useForm()
 
   const handleReset = () => {
-    form.resetFields();
-  };
-  
-  const changeDataInicio =(e) => {
+    form.resetFields()
+  }
+
+  const changeDataInicio = (e) => {
     setInicioZafra(e.target.value)
   }
-  const changeFinInicio =(e) => {
+  const changeFinInicio = (e) => {
     setFinZafra(e.target.value)
   }
 
-  // const changeData1 = (e) => {
-  //   const formatDate = new Date(e.$d);
-  //   const year = formatDate.toLocaleString("default", { year: "numeric" });
-  //   const month = formatDate.toLocaleString("default", { month: "2-digit" });
-  //   const day = formatDate.toLocaleString("default", { day: "2-digit" });
-
-  //   startDate = year + "-" + month + "-" + day;
-  // };
-  // const changeData2 = (e) => {
-  //   const formatDate = new Date(e.$d);
-  //   const year = formatDate.toLocaleString("default", { year: "numeric" });
-  //   const month = formatDate.toLocaleString("default", { month: "2-digit" });
-  //   const day = formatDate.toLocaleString("default", { day: "2-digit" });
-  //   endDate = year + "-" + month + "-" + day;
-  // };
+  const changeInicioDestileria = (e) => {
+    setInicioDestileria(e.target.value)
+  }
+  const changeFinDestileria = (e) => {
+    setFinDestileria(e.target.value)
+  }
 
   const handleSubmit = async (values) => {
     if (!dataRegisterEdit) {
-      createPeriodo(values);
+      createPeriodo(values)
     } else {
-      updatePeriodo(values);
+      updatePeriodo(values)
     }
-  };
+  }
   const createPeriodo = async (values) => {
-    values.inicio_zafra = inicioZafra;
-    values.fin_zafra = finZafra;
-
+    values.inicio_zafra = inicioZafra
+    values.fin_zafra = finZafra
+    values.inicio_destileria = inicioDestileria
+    values.fin_destileria = finDestileria
     try {
-      const res = await api("POST", "periodoZafra", values);
+      const res = await api('POST', 'periodoZafra', values)
       if (res.status === 201) {
-        setLoading(true);
+        setLoading(true)
         setTimeout(() => {
-          setLoading(false);
-          window.location.href = "/admin/periodos-zafra";
-        }, 2500);
+          setLoading(false)
+          window.location.href = '/admin/periodos-zafra'
+        }, 2500)
       }
       if (res?.response?.status === 400) {
-        handleReset();
-        const arraysError = res?.response?.data?.errors;
-        setMessageError(arraysError);
-        setDataError(true);
+        handleReset()
+        const arraysError = res?.response?.data?.errors
+        setMessageError(arraysError)
+        setDataError(true)
         setTimeout(() => {
-          setDataError(false);
-        }, 3000);
+          setDataError(false)
+        }, 3000)
       }
       if (res?.response?.status === 401) {
-        handleReset();
-        const arraysError = res?.response?.data?.errors;
-        setErrorPKDuplicate(arraysError);
-        setDataErrorPK(true);
+        handleReset()
+        const arraysError = res?.response?.data?.errors
+        setErrorPKDuplicate(arraysError)
+        setDataErrorPK(true)
         setTimeout(() => {
-          setDataErrorPK(false);
-        }, 3000);
+          setDataErrorPK(false)
+        }, 3000)
       }
     } catch (error) {
-      handleReset();
-      setServerError(true);
+      handleReset()
+      setServerError(true)
       setTimeout(() => {
-        setServerError(false);
-      }, 3000);
+        setServerError(false)
+      }, 3000)
     }
-  };
+  }
 
   const updatePeriodo = async (values) => {
-    values.inicio_zafra = inicioZafra;
-    values.fin_zafra = finZafra;
+    values.inicio_zafra = inicioZafra
+    values.fin_zafra = finZafra
+    values.inicio_destileria = inicioDestileria
+    values.fin_destileria = finDestileria
+
     try {
       const res = await api(
-        "PATCH",
+        'PATCH',
         `periodoZafra/${dataRegisterEdit?.id}`,
         values
-      );
+      )
       if (res.status === 200) {
-        setLoading(true);
+        setLoading(true)
         setTimeout(() => {
-          setLoading(false);
-          window.location.href = "/admin/periodos-zafra";
-        }, 2500);
+          setLoading(false)
+          window.location.href = '/admin/periodos-zafra'
+        }, 2500)
       }
       if (res?.response?.status === 400) {
-        const arraysError = res?.response?.data?.errors;
-        setMessageError(arraysError);
-        setDataError(true);
+        const arraysError = res?.response?.data?.errors
+        setMessageError(arraysError)
+        setDataError(true)
         setTimeout(() => {
-          setDataError(false);
-        }, 3000);
+          setDataError(false)
+        }, 3000)
       }
     } catch (error) {
-      setServerError(true);
+      setServerError(true)
       setTimeout(() => {
-        setServerError(false);
-      }, 3000);
+        setServerError(false)
+      }, 3000)
     }
-  };
+  }
 
-  let optionsAnios = [{ value: "", label: "Todos" }];
+  let optionsAnios = [{ value: '', label: 'Todos' }]
   aniosData?.forEach((d) => {
     const option = {
       value: d.id,
       label: d.anio_zafra,
-    };
-    optionsAnios.push(option);
-  });
+    }
+    optionsAnios.push(option)
+  })
 
-  let optionsIngenios = [{ value: "", label: "Todos" }];
+  let optionsIngenios = [{ value: '', label: 'Todos' }]
   ingeniosData?.forEach((d) => {
     const option = {
       value: d.id,
       label: d.nombre_ingenio,
-    };
-    optionsIngenios.push(option);
-  });
+    }
+    optionsIngenios.push(option)
+  })
 
   const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
+    console.log('Failed:', errorInfo)
+  }
   const dataEditInicio = new Date(dataRegisterEdit?.inicio_zafra)
-  const dataInicioConvertido =  moment(dataEditInicio).format('YYYY-MM-DD')
+  const dataInicioConvertido = moment(dataEditInicio).format('YYYY-MM-DD')
 
   const dataEditFin = new Date(dataRegisterEdit?.fin_zafra)
-  const dataFinConvertido =  moment(dataEditFin).format('YYYY-MM-DD')
-  
+  const dataFinConvertido = moment(dataEditFin).format('YYYY-MM-DD')
+
+  /*** FECHAS DESTILERIA ***/
+  const dataEditInicioDestileria = new Date(dataRegisterEdit?.inicio_destileria)
+  const dataInicioConvertidoDestileria = moment(
+    dataEditInicioDestileria
+  ).format('YYYY-MM-DD')
+
+  const dataEditFinDestileria = new Date(dataRegisterEdit?.fin_destileria)
+  const dataFinConvertidoDestileria = moment(dataEditFinDestileria).format(
+    'YYYY-MM-DD'
+  )
+
   return (
-    <div className="menuContainer">
+    <div className='menuContainer'>
       <Form
         form={form}
-        layout="vertical"
+        layout='vertical'
         labelCol={{
           span: 22,
         }}
@@ -168,7 +180,7 @@ const FormAddEditPeriodos = ({
           span: 22,
         }}
         style={{
-          marginLeft: "2vh",
+          marginLeft: '2vh',
         }}
         onFinish={handleSubmit}
         onFinishFailed={onFinishFailed}
@@ -180,21 +192,21 @@ const FormAddEditPeriodos = ({
         <Row>
           <Col xs={24} sm={11}>
             <Form.Item
-              label="Año"
-              name="id_anio_zafra"
-              style={{ width: "100%" }}
-              rules={[{ required: true, message: "Debe ingresar un año" }]}
+              label='Año'
+              name='id_anio_zafra'
+              style={{ width: '100%' }}
+              rules={[{ required: true, message: 'Debe ingresar un año' }]}
             >
               <Select
                 showSearch
                 style={{
-                  width: "100%",
+                  width: '100%',
                 }}
-                placeholder="Busque o seleccione"
+                placeholder='Busque o seleccione'
                 disabled={dataRegisterEdit ? true : false}
-                optionFilterProp="children"
+                optionFilterProp='children'
                 filterOption={(input, option) =>
-                  (option?.label ?? "").includes(input)
+                  (option?.label ?? '').includes(input)
                 }
                 options={optionsAnios}
               />
@@ -202,86 +214,89 @@ const FormAddEditPeriodos = ({
           </Col>
           <Col xs={24} sm={11}>
             <Form.Item
-              label="Ingenio"
-              name="id_nombre_ingenio"
-              style={{ width: "100%" }}
-              rules={[{ required: true, message: "Debe ingresar un ingenio" }]}
+              label='Ingenio'
+              name='id_nombre_ingenio'
+              style={{ width: '100%' }}
+              rules={[{ required: true, message: 'Debe ingresar un ingenio' }]}
             >
               <Select
                 showSearch
                 style={{
-                  width: "100%",
+                  width: '100%',
                 }}
-                placeholder="Busque o seleccione"
+                placeholder='Busque o seleccione'
                 disabled={dataRegisterEdit ? true : false}
-                optionFilterProp="children"
+                optionFilterProp='children'
                 filterOption={(input, option) =>
-                  (option?.label ?? "").includes(input)
+                  (option?.label ?? '').includes(input)
                 }
                 options={optionsIngenios}
               />
             </Form.Item>
           </Col>
-          {/* <Col xs={24} sm={11}>
-            <Form.Item
-              label="Fecha Inicio"
-              name="inicio_zafra"
-              style={{ width: "100%" }}
-              rules={[
-                { required: true, message: "Debe seleccionar una fecha" },
-              ]}
-            >
-              <DatePicker
-                style={{ width: "100%" }}
-                format={"DD/MM/YYYY"}
-                onChange={changeData1}
-              />
-            </Form.Item>
-          </Col> */}
+        </Row>
+        <Row>
           <Col xs={24} sm={11}>
-            <input type="date" defaultValue={`${dataInicioConvertido}`} onChange={changeDataInicio}/>
+            <p className='mb-1'>Inicio zafra</p>
+            <input
+              className='p-1 bg-white text-dark border border-1 input-date '
+              type='date'
+              defaultValue={`${dataInicioConvertido}`}
+              onChange={changeDataInicio}
+            />
           </Col>
           <Col xs={24} sm={11}>
-            <input type="date" defaultValue={`${dataFinConvertido}`} onChange={changeFinInicio}/>
+            <p className='mb-1'>Inicio zafra</p>
+            <input
+              className='p-1 bg-white text-dark border border-1 input-date '
+              type='date'
+              defaultValue={`${dataFinConvertido}`}
+              onChange={changeFinInicio}
+            />
           </Col>
-          {/* <Col xs={24} sm={11}>
-            <Form.Item
-              label="Fecha Finw"
-              name="fin_zafra"
-              style={{ width: "100%" }}
-              rules={[
-                { required: true, message: "Debe seleccionar una fecha" },
-              ]}
-            >
-              <DatePicker
-                style={{ width: "100%" }}
-                format={"DD/MM/YYYY"}
-                onChange={changeFinInicio}
-              />
-            </Form.Item>
-          </Col> */}
+        </Row>
+
+        <Row className='my-4'>
+          <Col xs={24} sm={11}>
+            <p className='mb-1'>Inicio destileria</p>
+            <input
+              className='p-1 bg-white text-dark border border-1 input-date'
+              type='date'
+              defaultValue={`${dataInicioConvertidoDestileria}`}
+              onChange={changeInicioDestileria}
+            />
+          </Col>
+          <Col xs={24} sm={11}>
+            <p className='mb-1'>Fin destileria</p>
+            <input
+              className='p-1 bg-white text-dark border border-1 input-date'
+              type='date'
+              defaultValue={`${dataFinConvertidoDestileria}`}
+              onChange={changeFinDestileria}
+            />
+          </Col>
         </Row>
         <Form.Item
-          className="text-end"
+          className='text-end mt-5'
           labelCol={{ span: 1 }}
           wrapperCol={{ span: 21 }}
         >
           {loading ? (
-            <Button disabled type="primary" htmlType="submit">
+            <Button disabled type='primary' htmlType='submit'>
               <Spin
-                as="span"
-                animation="border"
-                size="sm"
-                role="status"
-                aria-hidden="true"
+                as='span'
+                animation='border'
+                size='sm'
+                role='status'
+                aria-hidden='true'
               />
-              <span className="ms-2">
-                {dataRegisterEdit ? "Actualizando" : "Agregando"}
+              <span className='ms-2'>
+                {dataRegisterEdit ? 'Actualizando' : 'Agregando'}
               </span>
             </Button>
           ) : (
-            <Button type="primary" htmlType="submit">
-              {dataRegisterEdit ? "Actualizar" : "Agregar"}
+            <Button type='primary' htmlType='submit'>
+              {dataRegisterEdit ? 'Actualizar' : 'Agregar'}
             </Button>
           )}
         </Form.Item>
@@ -290,10 +305,10 @@ const FormAddEditPeriodos = ({
               <MsgError key={i} text2={e.path} text1={e.msg} />
             ))
           : null}
-        {dataErrorPK && <MsgError text1={"Error"} text2={errorPKDuplicate} />}
-        {serverError ? <MsgError text2="Server internal Error" /> : null}
+        {dataErrorPK && <MsgError text1={'Error'} text2={errorPKDuplicate} />}
+        {serverError ? <MsgError text2='Server internal Error' /> : null}
       </Form>
     </div>
-  );
-};
-export default FormAddEditPeriodos;
+  )
+}
+export default FormAddEditPeriodos
--- src/container/PeriodosZafra/PeriodoZafraContainer.js
+++ src/container/PeriodosZafra/PeriodoZafraContainer.js
@@ -1,90 +1,103 @@
-import { Container } from "react-bootstrap"
-import TitlePage from "../../components/TitlePages"
-import { useState, useEffect } from "react";
-import { getPeriodoZafra } from "../../utils/queryAPI/periodosZafra";
-import Spinn from "../../components/Spinner";
-import Filtros from "../../components/Filtros";
-import ListHeader from "../../components/ListHeader";
-import ListadoDatos from "../../components/ParteDiario/ListadoDatos";
-import { Button, Space } from "antd";
-import {
-  EditOutlined,
-} from "@ant-design/icons";
-import moment from "moment";
+import { Container } from 'react-bootstrap'
+import TitlePage from '../../components/TitlePages'
+import { useState, useEffect } from 'react'
+import { getPeriodoZafra } from '../../utils/queryAPI/periodosZafra'
+import Spinn from '../../components/Spinner'
+import Filtros from '../../components/Filtros'
+import ListHeader from '../../components/ListHeader'
+import ListadoDatos from '../../components/ParteDiario/ListadoDatos'
+import { Button, Space } from 'antd'
+import { EditOutlined } from '@ant-design/icons'
+import moment from 'moment'
 
-const PeriodoZafraContainer = ({tokenAuth, routeAPI}) => {
+const PeriodoZafraContainer = ({ tokenAuth, routeAPI }) => {
   const [periodosZafra, setPeriodosZafra] = useState(null)
-  const [yearZafra, setYearZafra] = useState(null);
+  const [yearZafra, setYearZafra] = useState(null)
   const [search, setSearch] = useState(null)
 
   useEffect(() => {
     dataPeriodosZafra()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[search, yearZafra])
+  }, [search, yearZafra])
 
-  const dataPeriodosZafra = async() => {
-    const params = {search, anio: yearZafra}
-    const data =  await getPeriodoZafra(params)
+  const dataPeriodosZafra = async () => {
+    const params = { search, anio: yearZafra }
+    const data = await getPeriodoZafra(params)
     setPeriodosZafra(data)
   }
 
   const columnsData = [
     {
-      key: "nombre_ingenio",
-      title: "Ingenio",
-      dataIndex: "nombre_ingenio",
-      defaultSortOrder: "ascend",
+      key: 'nombre_ingenio',
+      title: 'Ingenio',
+      dataIndex: 'nombre_ingenio',
+      defaultSortOrder: 'ascend',
       sorter: (a, b) => a.nombre_ingenio.localeCompare(b.nombre_ingenio),
     },
     {
-      key: "anio_zafra",
-      title: "Año",
-      dataIndex: "anio_zafra",
+      key: 'anio_zafra',
+      title: 'Año',
+      dataIndex: 'anio_zafra',
       // defaultSortOrder: "descend",
       sorter: (a, b) => a.anio_zafra - b.anio_zafra,
     },
     {
-      key: "inicio_zafra",
-      title: "Inicio",
-      dataIndex: "inicio_zafra",
-      defaultSortOrder: "descend",
+      key: 'inicio_zafra',
+      title: 'Inicio',
+      dataIndex: 'inicio_zafra',
+      defaultSortOrder: 'descend',
       sorter: (a, b) => new Date(a.inicio_zafra) - new Date(b.inicio_zafra),
-      render: (date) => date? moment(date).format('DD-MM-YYYY') : null,
+      render: (date) => (date ? moment(date).format('DD-MM-YYYY') : null),
     },
     {
-      key: "fin_zafra",
-      title: "Final",
-      dataIndex: "fin_zafra",
+      key: 'fin_zafra',
+      title: 'Final',
+      dataIndex: 'fin_zafra',
       sorter: (a, b) => new Date(a.inicio_zafra) - new Date(b.inicio_zafra),
-      render: (date) => date ? moment(date).format('DD-MM-YYYY') : null,
+      render: (date) => (date ? moment(date).format('DD-MM-YYYY') : null),
     },
     {
-      key: "actions",
-      title: "Acciones",
-      
+      key: 'inicio_destileria',
+      title: 'Inicio destileria',
+      dataIndex: 'inicio_destileria',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) =>
+        new Date(a.inicio_destileria) - new Date(b.inicio_destileria),
+      render: (date) => (date ? moment(date).format('DD-MM-YYYY') : null),
+    },
+    {
+      key: 'fin_destileria',
+      title: 'Fin destileria',
+      dataIndex: 'fin_destileria',
+      sorter: (a, b) => new Date(a.fin_destileria) - new Date(b.fin_destileria),
+      render: (date) => (date ? moment(date).format('DD-MM-YYYY') : null),
+    },
+    {
+      key: 'actions',
+      title: 'Acciones',
+
       render: (_, record) => {
         return (
-          <Space size="middle">
+          <Space size='middle'>
             <Button
-            className="bg-secondary btn-edit"
+              className='bg-secondary btn-edit'
               href={`/admin/periodos-zafra/editar/${record?.id}`}
-              style={{ color: "white" }}
-              icon={<EditOutlined className="" />}
+              style={{ color: 'white' }}
+              icon={<EditOutlined className='' />}
               title='Editar'
             ></Button>
           </Space>
-        );
+        )
       },
     },
-    
   ]
   return (
     <Container fluid>
-      <TitlePage titlePage={"Periodos de zafra"} />
-      <hr className="mx-3 mt-1" />
+      <TitlePage titlePage={'Periodos de zafra'} />
+      <hr className='mx-3 mt-1' />
       {periodosZafra === null ? (
-        <div className="d-flex justify-content-center align-items-center text-center">
-          <Spinn type="data" />
+        <div className='d-flex justify-content-center align-items-center text-center'>
+          <Spinn type='data' />
         </div>
       ) : (
         <>
@@ -95,17 +108,19 @@ const PeriodoZafraContainer = ({tokenAuth, routeAPI}) => {
             bandFilterSearch={true}
             placeHolderSearch='Valor'
           />
-          <ListHeader title={"Periodos de Zafra"} btnLink={'/admin/periodos-zafra/nuevo'}/>
-          <div className='pb-1 pt-3 px-4'>
-          <ListadoDatos
-            columns={columnsData}
-            scroll={30}
-            data={periodosZafra}
+          <ListHeader
+            title={'Periodos de Zafra'}
+            btnLink={'/admin/periodos-zafra/nuevo'}
           />
+          <div className='pb-1 pt-3 px-4'>
+            <ListadoDatos
+              columns={columnsData}
+              scroll={30}
+              data={periodosZafra}
+            />
           </div>
         </>
       )}
-
     </Container>
   )
 }
